a93fa82fa4e5761a62cd815794f9f8a9d38e9e9a
Jeff Behnke <jcbehnke.dev@gmail.com>
2025-02-26 19:25:23 +0000

    commiting changes 75% thru week 3, front end does not load

--- frontend-react-js/src/components/ProfileInfo.js
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -48,3 +48,45 @@ export default function ProfileInfo(props) {
     </div>
   )
 }
+
+/*
+import './ProfileInfo.css';
+import {ReactComponent as ElipsesIcon} from './svg/elipses.svg';
+import React from "react";
+
+import { Auth } from 'aws-amplify';
+
+const signOut = async () => {
+  try {
+      await Auth.signOut({ global: true });
+      window.location.href = "/"
+  } catch (error) {
+      console.log('error signing out: ', error);
+  }
+}
+
+  const classes = () => {
+    let classes = ["profile-info-wrapper"];
+    if (popped == true){
+      classes.push('popped');
+    }
+    return classes.join(' ');
+  }
+
+  return (
+    <div className={classes()}>
+      <div className="profile-dialog">
+        <button onClick={signOut}>Sign Out</button> 
+      </div>
+      <div className="profile-info" onClick={click_pop}>
+        <div className="profile-avatar"></div>
+        <div className="profile-desc">
+          <div className="profile-display-name">{props.user.display_name || "My Name" }</div>
+          <div className="profile-username">@{props.user.handle || "handle"}</div>
+        </div>
+        <ElipsesIcon className='icon' />
+      </div>
+    </div>
+  )
+}
+*/
